04dcd00df31ad42d57603a4f56db41feb4f7ae92
Lispectre <55454477+Lispectre@users.noreply.github.com>
2026-04-22 16:49:06 +0200

    no tarball

--- hosts/field/configuration.nix
+++ hosts/field/configuration.nix
@@ -5,7 +5,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
       ./disk-config.nix
     ];
 
